078d388b1093af207c2e0c5e8e2a177dcef43bc0
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-06-24 15:42:01 +0000

    implemented middle function to pinpoint midpoint of array

---
+++ middle.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅ Assertion Passed: arrays match`);
+  } else if (eqArrays(actual, expected) === false) {
+    console.log(`❌❌ Assertion Failed: arrays do not match`);
+  }
+};
+
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 !== 0) {
+    let middleIndex = Math.floor(array.length / 2);
+    return [array[middleIndex]];
+  } else if (array.length % 2 === 0) {
+    let middleIndex = array.length / 2;
+    return [array[middleIndex - 1], array[middleIndex]];
+  }
+};
+
+assertArraysEqual([], middle([])); //PASS
+assertArraysEqual([], middle([1, 2])); //PASS
+assertArraysEqual([], middle([1])); //PASS
+assertArraysEqual([], middle([1, 3, 5])); //FAIL
+assertArraysEqual([], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //FAIL
+assertArraysEqual([1, 7], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //FAIL
+assertArraysEqual([10], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //FAIL
+assertArraysEqual([1], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //PASS
+assertArraysEqual([], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //FAIL
+assertArraysEqual([8], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //FAIL
+assertArraysEqual([7, 3], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //FAIL
+assertArraysEqual([1, 7], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //PASS
