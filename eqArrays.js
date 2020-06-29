c593af89cade01096a5171c58a3cb96164c2078d
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-06-29 20:08:56 +0000

    Added modules to eqArrays.js. Added test file eqArraysTest.js with 'require'

--- eqArrays.js
+++ eqArrays.js
@@ -11,16 +11,4 @@ const eqArrays = function(actual, expected) {
   return true;
 };
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => should FAIL
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => should FAIL
+module.exports = eqArrays;
