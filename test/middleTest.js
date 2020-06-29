b31531e298298fecac4c5fce4b0697df35fc65de
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-06-29 21:44:40 +0000

    Incorporated Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,15 +1,41 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
+const assert = require('chai').assert; // 1
+const middle   = require('../middle');
 
-assertArraysEqual([], middle([])); //PASS
-assertArraysEqual([], middle([1, 2])); //PASS
-assertArraysEqual([], middle([1])); //PASS
-assertArraysEqual([], middle([1, 3, 5])); //FAIL
-assertArraysEqual([], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //FAIL
-assertArraysEqual([1, 7], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //FAIL
-assertArraysEqual([10], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //FAIL
-assertArraysEqual([1], middle([10, 4, 2, 7, 1, 7, 3, 20, 55])); //PASS
-assertArraysEqual([], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //FAIL
-assertArraysEqual([8], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //FAIL
-assertArraysEqual([7, 3], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //FAIL
-assertArraysEqual([1, 7], middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100])); //PASS
+describe("#middle", () => {
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns [] when []", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for [1, 3, 5]", () => {
+    assert.deepEqual(middle([1, 3, 5]), []);
+  });
+  it("returns [] for [10, 4, 2, 7, 1, 7, 3, 20, 55]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55]), []);
+  });
+  it("returns [1, 7] for [10, 4, 2, 7, 1, 7, 3, 20, 55, ]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55]), [1, 7]);
+  });
+  it("returns [10] for [10, 4, 2, 7, 1, 7, 3, 20, 55, 100]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55]), [10]);
+  });
+  it("returns [1] for [10, 4, 2, 7, 1, 7, 3, 20, 55, 100]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55]), [1]);
+  });
+  it("returns [] for [10, 4, 2, 7, 1, 7, 3, 20, 55, 100]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100]), []);
+  });
+  it("returns [8] for [10, 4, 2, 7, 1, 7, 3, 20, 55, 100]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100]), [8]);
+  });
+  it("returns [7, 3] for [10, 4, 2, 7, 1, 7, 3, 20, 55, 100]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100]), [7, 3]);
+  });
+  it("returns [1, 7] for [10, 4, 2, 7, 1, 7, 3, 20, 55, 100]", () => {
+    assert.deepEqual(middle([10, 4, 2, 7, 1, 7, 3, 20, 55, 100]), [1, 7]);
+  });
+});
