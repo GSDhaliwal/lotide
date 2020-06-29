4f992c56a98f11c0693eb597a811841a19579854
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-06-29 19:55:17 +0000

    Added modeules to assertEqual.js and head.js. Added test files assertEqualTest.js and headTest.js with 'require'

--- head.js
+++ head.js
@@ -2,19 +2,4 @@ const head = function(array) {
   return array[0];
 };
 
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]));
-assertEqual(head([5,6,7]), 7);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Labs");
+module.exports = head;
